aa1ed6e8f8ddd251bd39e768634b346cdcf56034
Aceheliflyer <aceheliflyer@gmail.com>
2017-11-09 14:00:21 -0500

    2.6.1-dev

--- commands/bot-staff/update.js
+++ commands/bot-staff/update.js
@@ -19,7 +19,7 @@ module.exports = class UpdateCommand extends Command {
     return this.client.isOwner(message.author)
   }
   async run (message) {
-    message.say('**Restart requested, please wait.**').then(function (e) {
+    message.say('**Update requested, please wait.**').then(function (e) {
       var result = childProcess.execSync('git pull').toString()
       message.say('```\n' + result + '\n```')
       if (result.indexOf('Already up-to-date.') > -1) {
