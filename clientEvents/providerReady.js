1f14d4bb873749615c03fec91c55cc3248e24bfb
Aceheliflyer <aceheliflyer@gmail.com>
2018-03-03 20:18:46 -0500

    Core: Add eventLogRefresh.

--- clientEvents/providerReady.js
+++ clientEvents/providerReady.js
@@ -1,5 +1,6 @@
-module.exports = (client, provider) => {
+module.exports = async (client, provider) => {
   client.log.info(`Initialized SQLite Provider!`, 'SQLite Initializer')
+
   setInterval(() => {
     var tempData = client.temp.sqlData
     client.temp.sqlData = []
@@ -11,8 +12,14 @@ module.exports = (client, provider) => {
         sqlUpdateMessage(client, tempData.length)
       }
     })
-  }, 1800000)
+  }, client.config.client.eventLogRefresh)
 }
+
+/**
+ * Sends a message to the terminal with the number of events sent to the database.
+ * @param {any} client The client of the application.
+ * @param {number} eventCount The amount of events.
+ */
 let sqlUpdateMessage = (client, eventCount) => {
   client.log.debug(`Updated database with ${eventCount} events.`, 'EVENT LOGGER')
 }
