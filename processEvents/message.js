99d85fece3091fc62c62834644bcd3072ca3fa95
Aceheliflyer <aceheliflyer@gmail.com>
2018-03-08 12:46:17 -0500

    Fix: Fix bot not shutting down.

--- processEvents/message.js
+++ processEvents/message.js
@@ -1,8 +1,8 @@
 module.exports = async (client, message) => {
   if (message === 'shutdown') {
     await client.log.info(`Shuting down!${client.shard ? ` | Shard ID: ${client.shard.id}` : ''}`, 'Client')
-    await client.provider.destroy()
-    await client.destroy()
-    await process.exit(0)
+    // Code to run before shutdown
+    // ...
+    process.exit(0)
   }
 }
